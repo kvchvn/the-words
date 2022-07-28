e37eba4357b24810da8a54bc63a5f49c3429d431
Anton Kachan <aa.kvchvn@gmail.com>
2022-07-28 23:42:44 +0300

    feat: improve onSignOut handler

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,22 +2,30 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { ROUTER_PATHS } from '../../constants';
+import { removeUserData } from '../../redux';
+import { useUserSelector } from '../../redux/selectors';
+import { useAppDispatch } from '../../redux/store';
 import { removeFromLocalStorage } from '../../utils';
 
 function Header() {
+  const { user } = useUserSelector();
+  const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
   const handleSignOut = () => {
     removeFromLocalStorage('user');
+    dispatch(removeUserData());
     navigate(ROUTER_PATHS.main);
   };
 
   return (
     <div>
       <h1>It`s Header</h1>
-      <button type="button" onClick={handleSignOut}>
-        Sign Out
-      </button>
+      {user && (
+        <button type="button" onClick={handleSignOut}>
+          Sign Out
+        </button>
+      )}
     </div>
   );
 }
