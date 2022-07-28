16dba5e39f712b575c1d84be04f6379fa675a69a
Anton Kachan <aa.kvchvn@gmail.com>
2022-07-28 21:55:49 +0300

    fix: fix imports

--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -1,19 +1,21 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { MainSignInResponse } from 'types';
 
-import { getFromLocalStorage } from 'utils';
+import { MainSignInResponse } from '../../types';
+import { getFromLocalStorage } from '../../utils';
 
-const initialState = getFromLocalStorage<MainSignInResponse>('user');
+const initialState = {
+  user: getFromLocalStorage<MainSignInResponse>('user'),
+};
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUserData: (state, action: PayloadAction<MainSignInResponse>) => {
-      state = action.payload;
+    setUserData: (state, { payload }: PayloadAction<MainSignInResponse>) => {
+      state.user = payload;
     },
   },
 });
 
-export const { setUserData } = userSlice.actions;
 export default userSlice.reducer;
+export const { setUserData } = userSlice.actions;
