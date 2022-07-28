d1bf6171246d62a0488faedb2cdc390f44468aa9
Anton Kachan <aa.kvchvn@gmail.com>
2022-07-28 23:40:03 +0300

    feat: update getUserFriendlyErrorMessage utility function

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,10 +2,11 @@ import { SERVER_ERROR } from '../constants';
 
 import { SerializedError } from '@reduxjs/toolkit';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
-import { ServerError } from '../types';
+import { ServerError, ServerErrorType } from '../types';
 
 export const getUserFriendlyErrorMessage = (
-  error: FetchBaseQueryError | SerializedError
+  error: FetchBaseQueryError | SerializedError,
+  type: keyof ServerErrorType
 ): string => {
   let status = 'default';
   if ('originalStatus' in error) {
@@ -13,5 +14,11 @@ export const getUserFriendlyErrorMessage = (
   } else if ('status' in error) {
     status = String(error.status);
   }
-  return status in SERVER_ERROR ? SERVER_ERROR[status as keyof ServerError] : SERVER_ERROR.default;
+
+  const message =
+    status in SERVER_ERROR ? SERVER_ERROR[status as keyof ServerError] : SERVER_ERROR.default;
+  if (typeof message === 'object') {
+    return message[type];
+  }
+  return message;
 };
