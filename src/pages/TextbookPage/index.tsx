fc1091acb4c8250be89a5637c5930d9c76685049
Anton Kachan <aa.kvchvn@gmail.com>
2022-07-29 23:13:14 +0300

    feat: implement base structure with logic of TextbookPage component

--- src/pages/TextbookPage/index.tsx
+++ src/pages/TextbookPage/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import { useGetWordsQuery } from '../../redux';
+import { MAX_GROUP, MAX_PAGE, MIN_PAGE } from '../../constants';
+
+import Loading from '../../components/Loading';
+import PageTitle from '../../components/PageTitle';
+import WordsList from '../../components/WordsList';
+import GroupSelect from '../../components/GroupSelect';
+import PageSelect from '../../components/PageSelect';
+
 function TextbookPage() {
-  return <h1>It`s Textbook Page</h1>;
+  const { data: wordsPage, isLoading } = useGetWordsQuery({ group: 0, page: 0 });
+
+  return (
+    <>
+      {isLoading && <Loading />}
+      <PageTitle>Учебник</PageTitle>
+      <GroupSelect count={MAX_GROUP} />
+      <PageSelect firstPage={MIN_PAGE} lastPage={MAX_PAGE} />
+      {wordsPage && <WordsList words={wordsPage} />}
+    </>
+  );
 }
 
 export default TextbookPage;
