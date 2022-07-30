2369e0af566102c6d95ee24398f6c882d24ba31a
Anton Kachan <aa.kvchvn@gmail.com>
2022-07-30 23:45:06 +0300

    feat: create custom redux selectors

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -1,5 +1,8 @@
 import { useSelector } from 'react-redux';
-
-import { RootState } from './store';
+import { RootState } from '.';
 
 export const useUserSelector = () => useSelector((state: RootState) => state.user);
+
+export const useGroupSelector = () => useSelector((state: RootState) => state.words.group);
+
+export const usePageSelector = () => useSelector((state: RootState) => state.words.page);
