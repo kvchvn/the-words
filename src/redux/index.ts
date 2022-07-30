46fc8d7fe3441774233d78b2e5bd3df135942055
Anton Kachan <aa.kvchvn@gmail.com>
2022-07-30 23:47:37 +0300

    feat: stand out RootStore, useAppDispatch

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,6 +1,15 @@
+import { useDispatch } from 'react-redux';
+
 import store from './store';
+import rootReducer from './reducer';
 import { useGetWordsQuery, useSignInUserMutation, useCreateUserMutation } from './slices/apiSlice';
 import { setUserData, removeUserData } from './slices/userSlice';
+import { useGroupSelector, usePageSelector, useUserSelector } from './selectors';
+
+type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export type RootState = ReturnType<typeof rootReducer>;
 
 export {
   store,
@@ -9,4 +18,7 @@ export {
   useCreateUserMutation,
   setUserData,
   removeUserData,
+  useGroupSelector,
+  usePageSelector,
+  useUserSelector,
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,21 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import saveToLocalStorage from './middlewares/words';
 
+import rootReducer from './reducer';
 import apiSlice from './slices/apiSlice';
-import userReducer from './slices/userSlice';
-import wordsListSliceReducer from './slices/wordsListSlice';
-import { useDispatch } from 'react-redux';
 
 const store = configureStore({
-  reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    user: userReducer,
-    words: wordsListSliceReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware, saveToLocalStorage),
 });
 
-type AppDispatch = typeof store.dispatch;
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-
-export type RootState = ReturnType<typeof store.getState>;
 export default store;
