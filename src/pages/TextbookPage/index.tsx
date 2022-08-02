ce77942bc3a7a20675a34ea32833a76e7804e97a
Anton Kachan <aa.kvchvn@gmail.com>
2022-08-02 16:48:52 +0300

    fix: fix TextbookPage component

--- src/pages/TextbookPage/index.tsx
+++ src/pages/TextbookPage/index.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
 
-import { useGetWordsQuery, useGroupSelector, usePageSelector } from '../../redux';
-import { DELTA, MAX_GROUP, MAX_PAGE, MIN_GROUP, MIN_PAGE } from '../../constants';
-
-import Loading from '../../components/Loading';
 import PageTitle from '../../components/PageTitle';
 import WordsList from '../../components/WordsList';
 import GroupSelect from '../../components/GroupSelect';
 import PageSelect from '../../components/PageSelect';
 
-function TextbookPage() {
-  const group = useGroupSelector();
-  const page = usePageSelector();
-  const { data: wordsPage, isLoading } = useGetWordsQuery({ group, page });
+import {
+  MAX_GROUP_FOR_GUESTS,
+  MAX_GROUP_FOR_USERS,
+  MAX_PAGE,
+  MIN_GROUP,
+  MIN_PAGE,
+} from '../../constants';
 
+function TextbookPage() {
   return (
     <>
-      {isLoading && <Loading />}
       <PageTitle>Учебник</PageTitle>
       <hr />
-      page: {page + DELTA}, group: {group + DELTA}
-      <hr />
-      <GroupSelect firstGroup={MIN_GROUP} lastGroup={MAX_GROUP} />
+      <GroupSelect
+        firstGroup={MIN_GROUP}
+        lastGroupForUsers={MAX_GROUP_FOR_USERS}
+        lastGroupForGuests={MAX_GROUP_FOR_GUESTS}
+      />
       <hr />
       <PageSelect firstPage={MIN_PAGE} lastPage={MAX_PAGE} />
       <hr />
-      {wordsPage && <WordsList words={wordsPage} />}
+      <WordsList />
     </>
   );
 }
