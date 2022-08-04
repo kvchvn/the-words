49a367433da40acb4852ff030d839d660f2fbf10
Anton Kachan <aa.kvchvn@gmail.com>
2022-08-04 15:11:16 +0300

    feat: implement logic of displaying a detailed info in Modal by clicking onto a word

--- src/components/WordsList/index.tsx
+++ src/components/WordsList/index.tsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 
-import { useWords } from '../../hooks';
+import { useModal, useWords } from '../../hooks';
+import { useAppDispatch, useWordIdSelector } from '../../redux';
+import { setCurrentWordId, unsetCurrentWordId } from '../../redux/slices/wordsListSlice';
 
 import Loading from '../Loading';
+import Modal from '../Modal';
+import WordCard from '../WordCard';
 
 function WordsList() {
+  const dispatch = useAppDispatch();
+  const currentWordId = useWordIdSelector();
   const { words, isLoading } = useWords();
+  const { isModalOpen, openModal, closeModal } = useModal();
+
+  const handleClose = useCallback(() => {
+    closeModal(() => dispatch(unsetCurrentWordId()));
+  }, [closeModal, dispatch]);
+
+  const handleOpen = useCallback(
+    (wordId: string) => {
+      openModal(() => dispatch(setCurrentWordId(wordId)));
+    },
+    [dispatch, openModal]
+  );
+
+  const currentWord = useMemo(() => {
+    if (words && currentWordId) {
+      return words.find((word) => word.id === currentWordId);
+    }
+  }, [words, currentWordId]);
 
   return (
-    <div>
-      {isLoading && <Loading />}
-      {words && (
-        <ul>
-          {words.map((word) => (
-            <li key={word.id}>{word.word}</li>
-          ))}
-        </ul>
+    <>
+      <div>
+        {isLoading && <Loading />}
+        {words && (
+          <ul>
+            {words.map((word) => (
+              <li key={word.id} onClick={handleOpen.bind(null, word.id)}>
+                {word.word}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      {isModalOpen && (
+        <Modal closeModal={handleClose}>
+          <WordCard word={currentWord} />
+        </Modal>
       )}
-    </div>
+    </>
   );
 }
 
