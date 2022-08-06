d0885c0e533b2bf6b6553693de4a4d43ecfe5951
Anton Kachan <aa.kvchvn@gmail.com>
2022-08-06 13:27:34 +0300

    feat: implement useDifficulty hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,5 +3,6 @@ import useGrouping from './useGrouping';
 import useWords from './useWords';
 import useSignOut from './useSignOut';
 import useModal from './useModal';
+import useDifficulty from './useDifficulty';
 
-export { usePagination, useGrouping, useWords, useSignOut, useModal };
+export { usePagination, useGrouping, useWords, useSignOut, useModal, useDifficulty };
