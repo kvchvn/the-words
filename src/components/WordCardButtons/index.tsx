189b4b6793a1be2545eca8c34e989483219ac83c
Anton Kachan <aa.kvchvn@gmail.com>
2022-08-06 13:29:29 +0300

    feat: create WordCardButtons component

---
+++ src/components/WordCardButtons/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { EASY_WORD, HARD_WORD } from '../../constants';
+import { useDifficulty } from '../../hooks';
+import { Word } from '../../types';
+
+interface WordCardButtonsProps {
+  wordData: Word;
+  difficulty: string | undefined;
+}
+
+function WordCardButtons({ wordData, difficulty }: WordCardButtonsProps) {
+  const { user, toggleDifficulty } = useDifficulty(wordData);
+
+  const toggleHardDifficulty = () => toggleDifficulty(difficulty, HARD_WORD);
+
+  const toggleEasyDifficulty = () => toggleDifficulty(difficulty, EASY_WORD);
+
+  return (
+    user && (
+      <div>
+        <button type="button" onClick={toggleHardDifficulty}>
+          Add to {HARD_WORD}
+        </button>
+        <button type="button" onClick={toggleEasyDifficulty}>
+          Add to {EASY_WORD}
+        </button>
+      </div>
+    )
+  );
+}
+
+export default WordCardButtons;
