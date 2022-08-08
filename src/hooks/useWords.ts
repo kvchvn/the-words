538086f0926234c71be42ef833ccc891bab60f2d
Anton Kachan <aa.kvchvn@gmail.com>
2022-08-08 11:15:21 +0300

    fix: fix useWords hook using aggregated words

--- src/hooks/useWords.ts
+++ src/hooks/useWords.ts
@@ -4,47 +4,57 @@ import {
   useLazyGetWordsQuery,
   useGroupSelector,
   useLazyGetUserWordsQuery,
+  useLazyGetAggregatedWordsQuery,
   usePageSelector,
   useUserSelector,
   useWordIdSelector,
 } from '../redux';
-import { HARD_WORD, MAX_GROUP_FOR_USERS } from '../constants';
+import { HARD_WORD, MAX_GROUP_FOR_USERS, WORDS_PER_PAGE as wordsPerPage } from '../constants';
+import { UserWord, Word, WordsResult, AggregatedWord } from '../types';
 
-const useWords = () => {
+interface UseWordsReturnType {
+  wordsResult: WordsResult;
+  currentWord: Word | UserWord | AggregatedWord | undefined;
+  isLoading: boolean;
+  hardUserWords: UserWord[] | undefined;
+}
+
+const useWords = (): UseWordsReturnType => {
   const group = useGroupSelector();
   const page = usePageSelector();
   const user = useUserSelector();
   const currentWordId = useWordIdSelector();
 
-  const [getAllWords, { data: wordsPage, isLoading }] = useLazyGetWordsQuery();
-  const [getUserWords, { data: userWords }] = useLazyGetUserWordsQuery();
+  const [getWords, { data: words, isLoading: isWordsLoading }] = useLazyGetWordsQuery();
+  const [getUserWords, { data: userWords, isLoading: isAggregatedWordsLoading }] =
+    useLazyGetUserWordsQuery();
+  const [getAggregatedWords, { data: aggregatedWords, isLoading: isUserWordsLoading }] =
+    useLazyGetAggregatedWordsQuery();
 
   useEffect(() => {
     if (user) {
       const { userId } = user;
-      getUserWords(userId);
+      if (group === MAX_GROUP_FOR_USERS) {
+        getUserWords(userId);
+      } else {
+        getAggregatedWords({ group, page, userId, wordsPerPage });
+      }
+    } else {
+      getWords({ group, page });
     }
-  }, [user, getUserWords, getAllWords]);
+  }, [user, group, page, getAggregatedWords, getUserWords, getWords]);
 
-  useEffect(() => {
-    if (group !== MAX_GROUP_FOR_USERS) {
-      getAllWords({ group, page });
-    }
-  }, [group, page, getAllWords]);
-
-  const checkWordDifficulty = (wordId: string) => {
-    return userWords?.find((userWord) => userWord.id === wordId)?.difficulty;
-  };
+  const isLoading = isWordsLoading || isAggregatedWordsLoading || isUserWordsLoading;
 
   const hardUserWords = userWords?.filter((word) => word.difficulty === HARD_WORD);
 
-  const words = group === MAX_GROUP_FOR_USERS ? hardUserWords : wordsPage;
+  const wordsResult = group === MAX_GROUP_FOR_USERS ? hardUserWords : aggregatedWords || words;
 
-  const currentWord = words?.find((word) => word.id === currentWordId);
+  const currentWord: Word | UserWord | AggregatedWord | undefined = wordsResult?.find(
+    (word) => word.id === currentWordId
+  );
 
-  const currentWordDifficulty = checkWordDifficulty(currentWordId);
-
-  return { words, userWords, currentWord, currentWordDifficulty, checkWordDifficulty, isLoading };
+  return { wordsResult, currentWord, isLoading, hardUserWords };
 };
 
 export default useWords;
