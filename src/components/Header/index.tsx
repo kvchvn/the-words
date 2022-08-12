1f3dcad528ec880e3c3ad8a3e44bf68ff7dd3c1e
Anton Kachan <aa.kvchvn@gmail.com>
2022-08-12 22:41:11 +0300

    feat: create basic layout of Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { useSignOut } from '../../hooks';
+import { Link } from 'react-router-dom';
 
 import { useUserSelector } from '../../redux';
+import { ROUTER_PATHS } from '../../constants';
 
 function Header() {
   const user = useUserSelector();
-  const { signOut } = useSignOut();
 
   return (
     <div>
       <h1>It`s Header</h1>
-      {user && (
-        <button type="button" onClick={signOut}>
-          Sign Out
-        </button>
-      )}
+      <nav>
+        <Link to={ROUTER_PATHS.textbook}>Учебник</Link>
+        <Link to={ROUTER_PATHS.sprintGame}>Спринт</Link>
+        <Link to={ROUTER_PATHS.audioCallGame}>Аудиовызов</Link>
+        <Link to={ROUTER_PATHS.authorization}>{user ? 'Профиль' : 'Авторизация'}</Link>
+      </nav>
     </div>
   );
 }
