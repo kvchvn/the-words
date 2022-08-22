7cc076e19e1322c6c44564e60d6a8bdac05e37db
Anton Kachan <aa.kvchvn@gmail.com>
2022-08-22 19:33:09 +0300

    feat: remove Suspense from index and add to Layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -7,7 +7,6 @@ import { ThemeProvider } from 'styled-components';
 import { store } from './redux';
 
 import App from './App';
-import Loading from './components/Loading';
 import GlobalStyles from './styles/global';
 import baseTheme from './styles/theme';
 
@@ -16,15 +15,13 @@ import './styles/index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Suspense fallback={<Loading />}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <ThemeProvider theme={baseTheme}>
-            <App />
-            <GlobalStyles />
-          </ThemeProvider>
-        </BrowserRouter>
-      </Provider>
-    </Suspense>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={baseTheme}>
+          <App />
+          <GlobalStyles />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
