5a9faec4167b5ac2278930b67e666b1408e77ee8
Anton Kachan <aa.kvchvn@gmail.com>
2022-09-03 16:43:21 +0300

    feat: implement playAudio utility function

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -25,3 +25,17 @@ export const getRandomBetween = (min: number, max: number) => {
   const modifiedMax = Math.floor(max);
   return Math.floor(Math.random() * (modifiedMax - modifiedMin + 1)) + modifiedMin;
 };
+
+export const playAudio = (src: string, playbackRate = 1) => {
+  return new Promise((resolve, reject) => {
+    const audio = new Audio(src);
+    audio.preload = 'auto';
+    audio.autoplay = true;
+    audio.playbackRate = playbackRate;
+    audio.onerror = () => reject();
+    audio.onended = () => {
+      audio.remove();
+      resolve(undefined);
+    };
+  });
+};
