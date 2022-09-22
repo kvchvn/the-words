09ad2584ffb3226f0808fee5a5d19e90132cf5c8
Anton Kachan <aa.kvchvn@gmail.com>
2022-09-22 10:45:33 +0300

    feat: create StyledHeader component via styled-components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom';
 
 import { FROM_MAIN, GAME_TYPES, ROUTER_PATHS } from '../../constants';
 import { useIsGameStartedSelector, useUserSelector } from '../../redux';
+import { StyledHeader } from './styles';
 
 function Header() {
   const user = useUserSelector();
   const isGameStarted = useIsGameStartedSelector();
 
   return isGameStarted ? null : (
-    <div>
+    <StyledHeader>
       <h1>Хэдэр</h1>
       <nav>
         <Link to={ROUTER_PATHS.textbook}>Учебник</Link>
@@ -29,7 +30,7 @@ function Header() {
         {user && <Link to={ROUTER_PATHS.statistic}>Статистика</Link>}
         <Link to={ROUTER_PATHS.authorization}>{user ? user.name : 'Авторизация'}</Link>
       </nav>
-    </div>
+    </StyledHeader>
   );
 }
 
