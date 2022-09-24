431e6efef3934ae6d085d4187f8afa33c33e5333
Anton Kachan <aa.kvchvn@gmail.com>
2022-09-24 03:16:07 +0300

    feat: add new font-families, change base font-family

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,8 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
+import { ThemeProps } from '../types';
+
 const GlobalStyles = createGlobalStyle`
   html {
-    font-family: 'Roboto Regular';
+    font-family: 'RoundedMplus1c Regular', 'Jura Regular', sans-serif;
+    
+    @media (min-width: ${({ theme }: ThemeProps<unknown>) => theme.device.tabletL}) {
+      font-size: ${({ theme }: ThemeProps<unknown>) => theme.size.xl};
+    }
+
+    @media (min-width: ${({ theme }: ThemeProps<unknown>) => theme.device.laptopM}) {
+      font-size: ${({ theme }: ThemeProps<unknown>) => theme.size.xxl};
+    }
   }
 `;
 
