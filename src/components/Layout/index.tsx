f65cf7c6f4c32ec23a4e36912cb3a9c2537f2384
Anton Kachan <aa.kvchvn@gmail.com>
2022-09-24 03:54:50 +0300

    feat: implement logic for Header menu

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,18 +1,25 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useRef } from 'react';
 
 import { Outlet } from 'react-router-dom';
 
 import Footer from '../Footer';
 import Header from '../Header';
 import Loading from '../Loading';
+import MenuToggler from '../MenuToggler';
+import { StyledMain } from './styles';
 
 function Layout() {
+  const menuToggler = useRef<HTMLInputElement>(null);
+
   return (
     <>
-      <Header />
-      <Suspense fallback={<Loading />}>
-        <Outlet />
-      </Suspense>
+      <MenuToggler forwardRef={menuToggler} />
+      <Header menuToggler={menuToggler} />
+      <StyledMain>
+        <Suspense fallback={<Loading />}>
+          <Outlet />
+        </Suspense>
+      </StyledMain>
       <Footer />
     </>
   );
