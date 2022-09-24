1ef57378a6e3ce5a3e4975d2386d5ec25635be46
Anton Kachan <aa.kvchvn@gmail.com>
2022-09-24 03:12:28 +0300

    feat: add size and device-size properties to baseTheme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -8,8 +8,26 @@ const baseTheme: Theme = {
     blue: '#286fb4',
     darkblue: '#13436b',
     gray: '#859eb9',
-    white: '#fffff7',
+    milk: '#fffff6',
     coral: '#df4c73',
+    orange: '#ffa500',
+  },
+  size: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '18px',
+    xl: '20px',
+    xxl: '24px',
+  },
+  device: {
+    mobileM: '320px',
+    mobileL: '480px',
+    tabletM: '800px',
+    tabletL: '1000px',
+    laptopS: '1200px',
+    laptopM: '1400px',
+    laptopL: '1920px',
   },
 };
 
--- src/types/common.ts
+++ src/types/common.ts
@@ -1,3 +1,5 @@
+import { ThemedStyledProps } from 'styled-components';
+
 export interface Theme {
   fontFamily: {
     primary: string;
@@ -6,10 +8,30 @@ export interface Theme {
     blue: string;
     darkblue: string;
     gray: string;
-    white: string;
+    milk: string;
     coral: string;
+    orange: string;
+  };
+  size: {
+    xs: string;
+    s: string;
+    m: string;
+    l: string;
+    xl: string;
+    xxl: string;
+  };
+  device: {
+    mobileM: string;
+    mobileL: string;
+    tabletM: string;
+    tabletL: string;
+    laptopS: string;
+    laptopM: string;
+    laptopL: string;
   };
 }
+
+export type ThemeProps<P> = ThemedStyledProps<P, Theme>;
 
 export interface RouterPaths {
   main: string;
