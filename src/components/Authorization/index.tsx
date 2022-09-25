eedd842013bc6eb4dc4151f8e90fe39ed9a3d0b9
Anton Kachan <aa.kvchvn@gmail.com>
2022-09-25 21:29:11 +0300

    feat: add styles for Authorization component

--- src/components/Authorization/index.tsx
+++ src/components/Authorization/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
 import { useToggle } from '../../hooks';
+import { StyledWrapper } from '../../styles/components';
+import AuthFormToggler from '../AuthFormToggler';
 import SignIn from '../SignIn';
 import SignUp from '../SignUp';
+import { StyledArticle, StyledImage, StyledSection } from './styles';
 
 function Authorization() {
   const { value: isSignUp, toggleValue } = useToggle();
 
   return (
-    <>{isSignUp ? <SignUp goToSignIn={toggleValue} /> : <SignIn goToSignUp={toggleValue} />}</>
+    <StyledSection>
+      <StyledWrapper>
+        <StyledImage />
+        <StyledArticle>
+          <AuthFormToggler isSignUp={isSignUp} toggleIsSignUp={toggleValue} />
+          {isSignUp ? <SignUp goToSignIn={toggleValue} /> : <SignIn />}
+        </StyledArticle>
+      </StyledWrapper>
+    </StyledSection>
   );
 }
 
