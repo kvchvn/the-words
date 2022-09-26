983de7eb1c524eb1f27f99d502b94ba0f1d07cfe
Anton Kachan <aa.kvchvn@gmail.com>
2022-09-26 19:54:29 +0300

    fix: fix styles at Textbook page

--- src/pages/TextbookPage/index.tsx
+++ src/pages/TextbookPage/index.tsx
@@ -15,7 +15,7 @@ import {
   MIN_PAGE,
   ROUTER_PATHS,
 } from '../../constants';
-import { StyledPageTitle } from '../../styles/components';
+import { StyledPageTitle, StyledWrapper } from '../../styles/components';
 
 function TextbookPage() {
   const [isGamesDisabled, setIsGamesDisabled] = useState(false);
@@ -24,31 +24,34 @@ function TextbookPage() {
 
   return (
     <>
-      <StyledPageTitle>Учебник</StyledPageTitle>
-      <hr />
-      <GroupSelect
-        firstGroup={MIN_GROUP}
-        lastGroupForUsers={MAX_GROUP_FOR_USERS}
-        lastGroupForGuests={MAX_GROUP_FOR_GUESTS}
-      />
-      <hr />
-      <PageSelect firstPage={MIN_PAGE} lastPage={MAX_PAGE} />
-      <hr />
-      <WordsList toggleGames={toggleGames} />
-      <nav>
-        <Link
-          to={`/${ROUTER_PATHS.gameWelcome}`}
-          state={{ entry: FROM_TEXTBOOK, game: GAME_TYPES.sprintGame }}
-        >
-          <button disabled={isGamesDisabled}>Спринт</button>
-        </Link>
-        <Link
-          to={`/${ROUTER_PATHS.gameWelcome}`}
-          state={{ entry: FROM_TEXTBOOK, game: GAME_TYPES.audioCallGame }}
-        >
-          <button disabled={isGamesDisabled}>Аудиовызов</button>
-        </Link>
-      </nav>
+      <StyledPageTitle>
+        <StyledWrapper>Учебник</StyledWrapper>
+      </StyledPageTitle>
+      <section>
+        <StyledWrapper>
+          <GroupSelect
+            firstGroup={MIN_GROUP}
+            lastGroupForUsers={MAX_GROUP_FOR_USERS}
+            lastGroupForGuests={MAX_GROUP_FOR_GUESTS}
+          />
+          <PageSelect firstPage={MIN_PAGE} lastPage={MAX_PAGE} />
+          <WordsList toggleGames={toggleGames} />
+          <nav>
+            <Link
+              to={`/${ROUTER_PATHS.gameWelcome}`}
+              state={{ entry: FROM_TEXTBOOK, game: GAME_TYPES.sprintGame }}
+            >
+              <button disabled={isGamesDisabled}>Спринт</button>
+            </Link>
+            <Link
+              to={`/${ROUTER_PATHS.gameWelcome}`}
+              state={{ entry: FROM_TEXTBOOK, game: GAME_TYPES.audioCallGame }}
+            >
+              <button disabled={isGamesDisabled}>Аудиовызов</button>
+            </Link>
+          </nav>
+        </StyledWrapper>
+      </section>
     </>
   );
 }
