c456da66ce33a165046cc0663388362db4cf60e2
Anton Kachan <aa.kvchvn@gmail.com>
2022-10-09 02:18:22 +0300

    feat: add styles to SprintGamePage

--- src/pages/SprintGamePage/index.tsx
+++ src/pages/SprintGamePage/index.tsx
@@ -8,7 +8,8 @@ import Timer from '../../components/Timer';
 import { FROM_MAIN, FROM_TEXTBOOK, GAME_ROUND_TIME } from '../../constants';
 import { useGame, useWordStatistic } from '../../hooks';
 import useSprintGame from '../../hooks/useSprintGame';
-import { StyledPageTitle } from '../../styles/components';
+import { StyledWrapper } from '../../styles/components';
+import { StyledBox } from './styles';
 
 interface SprintGamePageLocation {
   state: typeof FROM_MAIN | typeof FROM_TEXTBOOK | null;
@@ -26,20 +27,19 @@ function SprintGamePage() {
   const { updateWordStatistic } = useWordStatistic('SPRINT');
 
   return (
-    <>
-      <StyledPageTitle>Спринт</StyledPageTitle>
-      <div>
+    <StyledWrapper>
+      <StyledBox>
         <Timer range={GAME_ROUND_TIME} finishGame={finishGame} />
         <Score />
-        <SprintRound
-          originalWord={originalWord}
-          isGameOver={isGameOver}
-          playRoundSound={playRoundSound}
-          showNextWord={toNextWord}
-          updateWordStatistic={updateWordStatistic}
-        />
-      </div>
-    </>
+      </StyledBox>
+      <SprintRound
+        originalWord={originalWord}
+        isGameOver={isGameOver}
+        playRoundSound={playRoundSound}
+        showNextWord={toNextWord}
+        updateWordStatistic={updateWordStatistic}
+      />
+    </StyledWrapper>
   );
 }
 
---
+++ src/pages/SprintGamePage/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledBox = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  max-width: 25rem;
+`;
