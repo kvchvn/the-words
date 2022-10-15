676dcca2a3eaa81633c68b128121c88f0f399d71
Anton Kachan <aa.kvchvn@gmail.com>
2022-10-15 13:52:17 +0300

    feat: add ability to abort a game

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { ROUTER_PATHS } from '../../constants';
+import { useIsGameOverSelector } from '../../redux';
 import { StyledArticle, StyledBox, StyledButton, StyledIcon, StyledTime } from './styles';
 
 interface TimerProps {
@@ -12,17 +13,18 @@ interface TimerProps {
 
 function Timer({ range, finishGame }: TimerProps) {
   const [time, setTime] = useState(range);
+  const isGameOver = useIsGameOverSelector();
   const navigate = useNavigate();
 
   const goToResults = () => navigate(`/${ROUTER_PATHS.gameResults}`);
 
   useEffect(() => {
-    if (time > 0) {
+    if (time > 0 && !isGameOver) {
       setTimeout(() => setTime(time - 1), 1000);
     } else {
       finishGame();
     }
-  }, [time, finishGame]);
+  }, [time, finishGame, isGameOver]);
 
   return (
     <StyledArticle>
@@ -30,10 +32,10 @@ function Timer({ range, finishGame }: TimerProps) {
         <StyledIcon />
         <StyledTime time={time}>{time !== 0 ? time : 'Время вышло!'}</StyledTime>
       </StyledBox>
-      {time === 0 && (
-        <StyledButton type="button" onClick={goToResults}>
-          <span>Результат</span>
-        </StyledButton>
+      {time === 0 || isGameOver ? (
+        <StyledButton onClick={goToResults}>Перейти к результатам</StyledButton>
+      ) : (
+        <StyledButton onClick={finishGame}>Завершить игру</StyledButton>
       )}
     </StyledArticle>
   );
