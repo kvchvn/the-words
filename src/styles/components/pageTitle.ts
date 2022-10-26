7d9c5b62156a2c0f69e419b084e2f390ac381e31
Anton Kachan <aa.kvchvn@gmail.com>
2022-10-26 23:03:40 +0300

    fix: change StyledPageTitle tag from h2 to h1

--- src/styles/components/pageTitle.ts
+++ src/styles/components/pageTitle.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
-const StyledPageTitle = styled.h2`
+const StyledPageTitle = styled.h1`
   padding: 1rem 0;
-  letter-spacing: 1px;
+  letter-spacing: 2px;
+  width: 100%;
   font-family: 'Jura Regular', 'RoundedMplus1c Regular', 'sans-serif';
-  font-size: 2rem;
+  font-size: 2.5rem;
 `;
 
 export default StyledPageTitle;
