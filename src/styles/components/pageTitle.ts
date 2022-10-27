e29b9c69bfb9c388c47046f5ac1f48cd24295295
Anton Kachan <aa.kvchvn@gmail.com>
2022-10-27 23:23:12 +0300

    fix: change font-family of StyledPageTitle

--- src/styles/components/pageTitle.ts
+++ src/styles/components/pageTitle.ts
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 const StyledPageTitle = styled.h1`
   padding: 1rem 0;
-  letter-spacing: 2px;
+  letter-spacing: 3px;
   width: 100%;
-  font-family: 'Jura Regular', 'RoundedMplus1c Regular', 'sans-serif';
+  font-family: 'Pacifico Regular', 'RoundedMplus1c Regular', 'sans-serif';
+  font-weight: 400;
   font-size: 2.5rem;
 `;
 
