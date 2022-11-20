71a5e2f33032a6e3aa0173c89ea43687f42e0f84
Anton Kachan <aa.kvchvn@gmail.com>
2022-11-20 23:02:56 +0300

    feat: change common image to progressive one at NotFoundPage

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import not_found from '../../assets/img/not_found.webp';
+import not_found_tiny from '../../assets/img/not_found_tiny.webp';
+import Image from '../../components/Image';
 import { ROUTER_PATHS } from '../../constants';
-import { StyledImage, StyledLink, StyledSection } from './styles';
+import { StyledLink, StyledSection } from './styles';
 
 function NotFoundPage() {
   return (
     <StyledSection>
-      <StyledImage src="" alt="" />
+      <Image src={not_found} placeholder={not_found_tiny} alt="404" type="notFound" />
       <h3>Такая страница не найдена</h3>
       <StyledLink to={ROUTER_PATHS.main}>
         <p>На главную</p>
